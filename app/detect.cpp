a11b011e6cfdde77ad4e6dd5fe46cb1d0279c277
jayprajapati009 <jayprajapati9900@gmail.com>
2022-10-19 21:17:14 -0400

    Code Upload

---
+++ app/detect.cpp
@@ -0,0 +1,244 @@
+/******************************************************************************
+ * MIT License
+ * 
+ * Copyright (c) 2022 Anukriti Singh, Jay Prajapati, and Shail Shah
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE. 
+ ******************************************************************************/
+
+/**
+ * @file cam_bot.hpp
+ * @author Phase 1 - Anukriti Singh (design keeper), Jay Prajapati (driver), and Shail Shah (navigator)
+ * @brief cam_bot class declaration for Acme Robotics - Human Tracker
+ * @version 0.1
+ * 
+ * @copyright MIT License
+ * 
+ */
+
+
+#include <detect.hpp>
+
+
+acme_robots::Detector::Detector(double conf, const std::vector<std::string> &classes) {
+    /// initializes default parameter and creates the model network
+    InitModel(conf, classes);
+
+    /// run the model once before actually performing detection on video frame
+    WarmUp();
+}
+acme_robots::Detector::~Detector() {}
+
+std::vector<acme_robots::Detection> acme_robots::Detector::Detect(const cv::Mat& frame) {
+    /// create a vector to store Detection objects
+    std::vector<acme_robots::Detection> detections = {};
+
+    /// create a Mat to store the input blob
+    cv::Mat b;
+    if ( !frame.empty() ) {
+        /// create a blob for the model to run a forward pass
+        b = cv::dnn::blobFromImage(frame, scale_, size_, mean_, swap_, crop_);
+
+        /// set input for the model
+        network_.setInput(b);
+        outputs_.clear();
+
+        /// run a forward pass
+        network_.forward(outputs_, out_names_);
+        cv::Size img_size = frame.size();
+
+        /// process the outputs of the model forward pass
+        detections = ProcessNet(img_size);
+    }
+    return detections;
+}
+
+void acme_robots::Detector::Classes(const std::vector<std::string> &classes) {
+    classes_ = classes;
+}
+
+void acme_robots::Detector::NmsThresh(const double nms_thresh) {
+    nms_thresh_ = nms_thresh;
+}
+
+void acme_robots::Detector::InputWIdth(const int input_width) {
+    input_width_ = input_width;
+}
+
+void acme_robots::Detector::InputHeight(const int input_height) {
+    input_height_ = input_height;
+}
+
+void acme_robots::Detector::ScaleFactor(const double scale_factor) {
+    scale_ = scale_factor;
+}
+
+void acme_robots::Detector::SwapRB(const bool swap_rb) {
+    swap_ = swap_rb;
+}
+
+void acme_robots::Detector::MeanToSubtract(const cv::Scalar &mean) {
+    mean_ = mean;
+}
+
+void acme_robots::Detector::CropImg(const bool crop_img) {
+    crop_ = crop_img;
+}
+
+void acme_robots::Detector::Backend(const int backend) {
+    backend_ = backend;
+}
+
+void acme_robots::Detector::Target(const int target) {
+    target_ = target;
+}
+
+void acme_robots::Detector::NumChannels(const int num_channels) {
+    num_channels_ = num_channels;
+}
+
+void acme_robots::Detector::InitModel(double conf, const std::vector<std::string> &c) {
+    /// set confidence threshold
+    conf_thresh_ = conf;
+
+    /// set classes to detect by the Detector class object
+    classes_ = c;
+
+    /// set nms threshold default value
+    nms_thresh_ = 0.4;
+
+    /// set input width default value
+    input_width_ = 416;
+
+    /// set input height default value
+    input_height_ = 416;
+
+    /// set input img size
+    size_ = cv::Size(input_width_, input_height_);
+
+    /// set scale factor default value
+    scale_ = 0.00392157;
+
+    /// set mean to subtract default value
+    mean_ = cv::Scalar();
+
+    /// set swap red and blue channels default value
+    swap_ = true;
+
+    /// set random crop img default value
+    crop_ = false;
+
+    /// set backend default value for the model
+    backend_ = 0;
+
+    /// set target default value for the model
+    target_ = 0;
+
+    /// set num of channels default value for the model
+    num_channels_ = 3;
+
+    /// set classes default value for the model
+    all_classes_ = {"person", "bicycle", "car", "motorbike", "aeroplane",
+     "bus", "train", "truck", "boat", "traffic light", "fire hydrant",
+      "stop sign", "parking meter", "bench", "bird", "cat", "dog", "horse",
+      "sheep", "cow", "elephant", "bear", "zebra", "giraffe", "backpack",
+      "umbrella", "handbag", "tie", "suitcase", "frisbee", "skis", "snowboard",
+      "sports ball", "kite", "baseball bat", "baseball glove", "skateboard",
+      "surfboard", "tennis racket", "bottle", "wine glass", "cup", "fork",
+      "knife", "spoon", "bowl", "banana", "apple", "sandwich", "orange",
+      "broccoli", "carrot", "hot dog", "pizza", "donut", "cake", "chair",
+      "sofa", "pottedplant", "bed", "diningtable", "toilet", "tvmonitor",
+      "laptop", "mouse", "remote", "keyboard", "cell phone", "microwave",
+      "oven", "toaster", "sink", "refrigerator", "book", "clock", "vase",
+      "scissors", "teddy bear", "hair drier", "toothbrush" };
+
+    /// set weights file path
+    std::string weights_path = "..//data//yolov4-tiny.weights";
+
+    /// set config file path
+    std::string config_path = "..//data//yolov4-tiny.cfg";
+
+    /// create a model network
+    network_ = cv::dnn::readNet(weights_path, config_path);
+
+    /// set network backend using the set backend value
+    network_.setPreferableBackend(backend_);
+
+    /// set network target using the set target value
+    network_.setPreferableTarget(target_);
+
+    /// get output layer names for the model
+    out_names_ = network_.getUnconnectedOutLayersNames();
+}
+
+void acme_robots::Detector::WarmUp() {
+    /// create a temporary Mat object
+    cv::Mat temp = cv::Mat::zeros(cv::Size(416, 416), CV_8UC3);
+
+    /// run Detector on the temporary Mat as a warmup
+    Detect(temp);
+}
+
+std::vector<acme_robots::Detection> acme_robots::Detector::ProcessNet(const cv::Size &s) {
+    std::vector<acme_robots::Detection> detections;
+    std::vector<std::string> class_names;
+    std::vector<float> confidences;
+    std::vector<cv::Rect> bboxes;
+    std::vector<int> indices;
+
+    for ( cv::Mat &output : outputs_ ) {
+        auto *data = reinterpret_cast<float*>(output.data);
+        for ( int i = 0 ; i < output.rows ; i++, data += output.cols ) {
+            int x_center =  static_cast<int>(data[0]*s.width);
+            int y_center = static_cast<int>(data[1]*s.height);
+            cv::Mat scores = output.row(i).colRange(5, output.cols);
+            cv::Point class_id_point;
+            double confidence;
+            cv::minMaxLoc(scores, 0, &confidence, 0, &class_id_point);
+            if ( confidence > conf_thresh_ ) {
+                int class_id = class_id_point.x;
+                std::string cl4ss = all_classes_[class_id];
+                auto iter = std::find(classes_.begin(), classes_.end(), cl4ss);
+                if ( iter != classes_.end() ) {
+                    x_center = std::max(0, x_center);
+                    y_center =  std::max(0, y_center);
+                    int width = static_cast<int>(data[2]*s.width);
+                    int height = static_cast<int>(data[3]*s.height);
+                    int x_left = x_center - width / 2;
+                    int y_top = y_center - height / 2;
+                    cv::Rect bbox(x_left, y_top, width, height);
+                    class_names.push_back(cl4ss);
+                    confidences.push_back(static_cast<float>(confidence));
+                    bboxes.push_back(bbox);
+                }
+            }
+        }
+    }
+
+    cv::dnn::NMSBoxes(bboxes, confidences, conf_thresh_, nms_thresh_, indices);
+
+    for ( int index : indices ) {
+        cv::Rect bbox = bboxes[index];
+        double conf = static_cast<double>(confidences[index]);
+        std::string name = class_names[index];
+        Detection box(bbox, conf, name);
+        detections.push_back(box);
+    }
+    return detections;
+}
---
+++ test/test.cpp
@@ -0,0 +1,187 @@
+/******************************************************************************
+ * MIT License
+ * 
+ * Copyright (c) 2021 Mayank Joshi, Naitri Rajyaguru
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE. 
+ ******************************************************************************/
+
+/**
+ * @file test.cpp
+ * @author Phase 1 - Mayank Joshi (driver) and Naitri Rajyaguru (navigator)
+ * @brief Unit Tests for Acme Robotics - Human Tracker
+ * @version 0.1
+ * 
+ * @copyright MIT License
+ * 
+ */
+
+#include <gtest/gtest.h>
+#include <opencv2/opencv.hpp>
+#include <opencv2/core.hpp>
+
+
+#include <cam_bot.hpp>
+#include <track.hpp>
+#include <detect.hpp>
+#include <Utils.hpp>
+
+
+acme_robots::cam_bot robot_object(0, -1);
+acme_robots::Track tracker_object(0.1);
+std::vector<std::string> classes = {"person"};
+acme_robots::Detector detect_object(0.2, classes);
+acme_robots::Utils utils_object;
+
+TEST(cam_bot, Run) {
+    acme_robots::Mode mode = acme_robots::Mode::RealTime;
+    robot_object.TestCounter(1);
+    robot_object.ShowWindow(false);
+    ASSERT_NO_THROW(robot_object.Run(mode));
+
+    mode = acme_robots::Mode::Testing;
+    ASSERT_NO_THROW(robot_object.Run(mode));
+
+    mode = acme_robots::Mode::Training;
+    ASSERT_NO_THROW(robot_object.Run(mode));
+}
+// TEST(AutoBot, SetRobotPose) {
+//     acme::Pose p;
+//     ASSERT_NO_THROW(robot_object.SetRobotPose(p));
+// }
+// TEST(AutoBot, SetCameraPose) {
+//     acme::Pose p;
+//     ASSERT_NO_THROW(robot_object.SetCameraPose(p));
+// }
+TEST(cam_bot, FocalLength) {
+    double fl = 0.036;
+    ASSERT_NO_THROW(robot_object.FocalLength(fl));
+}
+TEST(cam_bot, ProcessingSize) {
+    int w = 416;
+    int h = 416;
+    cv::Size s(w, h);
+    ASSERT_NO_THROW(robot_object.ProcessingSize(w, h));
+    ASSERT_NO_THROW(robot_object.ProcessingSize(s));
+}
+// TEST(cam_bot, GetObjects) {
+//     ASSERT_NEAR(10, static_cast<int>(robot_object.GetObjects().size()), 20);
+// }
+TEST(cam_bot, HumanHeight) {
+    double height = 1.6;
+    ASSERT_NO_THROW(robot_object.HumanHeight(height));
+}
+
+TEST(track, TrackHumans) {
+    cv::Mat img = cv::imread("..//data//test.png");
+    auto output = tracker_object.TrackHumans(img);
+    ASSERT_EQ(static_cast<int>(output.size()), 1);
+}
+TEST(Detection, structure) {
+    std::string class_name = "person";
+    acme_robots::Detection temp_object(cv::Rect(1, 1, 1, 1), 0.4, class_name);
+
+    ASSERT_EQ(static_cast<int>(temp_object.bbox.x), 1);
+    ASSERT_EQ(static_cast<int>(temp_object.bbox.y), 1);
+    ASSERT_EQ(static_cast<int>(temp_object.bbox.width), 1);
+    ASSERT_EQ(static_cast<int>(temp_object.bbox.height), 1);
+    ASSERT_EQ(temp_object.confidence, 0.4);
+    ASSERT_EQ(temp_object.name, class_name);
+}
+TEST(Detector, Detect) {
+    cv::Mat img = cv::imread("..//data//test.png");
+    auto output = detect_object.Detect(img);
+    ASSERT_EQ(static_cast<int>(output.size()), 1);
+}
+TEST(Detector, ClassesToDetect) {
+    ASSERT_NO_THROW(detect_object.Classes());
+}
+TEST(Detector, NmsThresh) {
+    ASSERT_NO_THROW(detect_object.NmsThresh(0.4));
+}
+TEST(Detector, InputWIdth) {
+    ASSERT_NO_THROW(detect_object.InputWIdth(640));
+}
+TEST(Detector, InputHeight) {
+    ASSERT_NO_THROW(detect_object.InputHeight(480));
+}
+TEST(Detector, SwapRB) {
+    ASSERT_NO_THROW(detect_object.SwapRB(true));
+}
+TEST(Detector, CropImg) {
+    ASSERT_NO_THROW(detect_object.CropImg(false));
+}
+TEST(Detector, Backend) {
+    ASSERT_NO_THROW(detect_object.Backend(0));
+}
+TEST(Detector, Target) {
+    ASSERT_NO_THROW(detect_object.Target(0));
+}
+TEST(Detector, NumChannels) {
+    ASSERT_NO_THROW(detect_object.NumChannels(3));
+}
+TEST(Detector, ScaleFactor) {
+    double sf = 1.0;
+    ASSERT_NO_THROW(detect_object.ScaleFactor(sf));
+}
+TEST(Detector, MeanToSubtract) {
+    cv::Scalar mean = cv::Scalar();
+    ASSERT_NO_THROW(detect_object.MeanToSubtract(mean));
+}
+
+TEST(Utils, DrawBbox) {
+    cv::Mat img = cv::Mat::zeros(cv::Size(100, 100), CV_8UC3);
+    cv::Rect box(2, 2, 2, 2);
+    std::string label = "";
+    auto output  = utils_object.DrawBbox(img, box, label);
+    ASSERT_EQ(static_cast<int>(output.cols), 100);
+    ASSERT_EQ(static_cast<int>(output.rows), 100);
+}
+TEST(Utils, GetBboxCenter) {
+    cv::Rect r = cv::Rect(0, 0, 10, 10);
+    auto output = utils_object.GetBboxCenter(r);
+    ASSERT_EQ(output.x, 5);
+    ASSERT_EQ(output.y, 5);
+}
+TEST(Utils, GetBboxArea) {
+    cv::Rect r = cv::Rect(0, 0, 10, 10);
+    auto output = utils_object.GetBboxArea(r);
+    ASSERT_EQ(output, 100.0);
+}
+TEST(Utils, ResizeImage) {
+    cv::Mat img = cv::Mat::zeros(cv::Size(10, 10), CV_8UC3);
+    cv::Size s = cv::Size(20, 20);
+    auto output  = utils_object.ResizeImage(img, s);
+    ASSERT_EQ(static_cast<int>(output.cols), 20);
+    ASSERT_EQ(static_cast<int>(output.rows), 20);
+}
+// TEST(Utils, CalculateIOU) {
+//     cv::Rect r1 = cv::Rect(0, 0, 10, 10);
+//     cv::Rect r2 = cv::Rect(5, 5, 10, 10);
+//     auto output = utils_object.CalculateIOU(r1, r2);
+//     ASSERT_NEAR(output, 0.166, 1);
+// }
+// TEST(Utils, PixelsToPose) {
+//     cv::Rect r = cv::Rect(0, 0, 10, 10);
+//     double calib_factor = 0.2;
+//     auto output = utils_object.PixelsToPose(r, calib_factor);
+//     ASSERT_NEAR(output.x, 0.02, 0.1);
+//     ASSERT_NEAR(output.y, 5.0, 0.1);
+//     ASSERT_NEAR(output.z, 5.0, 0.1);
+// }
